c0bc580429959fcfa3522571a7063f77af0eca11
Jonathan Lynch <lynch.jonathanm@gmail.com>
2018-09-18 15:22:56 -0400

    Week 4 class progress

---
+++ week_04/case_study/CaseStudy3.R
@@ -0,0 +1,6 @@
+library(gapminder)
+library(dplyr)
+library(ggplot2)
+filter(gapminder, country != "Kuwait") %>% 
+group_by(continent, year) %>% 
+summarise(gdpPercap=mean(x=gdpPercap, w=pop), pop=sum(as.numeric(pop)))
