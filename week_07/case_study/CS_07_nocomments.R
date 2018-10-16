d93747378d66db862ab29adaeb2b9b3072848ff3
Jonathan Lynch <lynch.jonathanm@gmail.com>
2018-10-16 14:16:01 -0400

    10/16 - class work

--- week_07/case_study/CS_07_nocomments.R
+++ week_07/case_study/CS_07_nocomments.R
@@ -1,2 +1,10 @@
+library(tidyverse)
+library(reprex)
+library(sf)
+library(spData)
+library(dplyr)
+
+data(world)
+
 ggplot(world,aes(x=gdpPercap, y=continent, color=continent))+
    geom_density(alpha=0.5,color=F)
