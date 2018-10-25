c4cd329935a777cd7fb1c9b931be9014a001908d
Jonathan Lynch <lynch.jonathanm@gmail.com>
2018-10-25 16:05:08 -0400

    10/25 - refresh

--- week_04/case_study/CaseStudy3.R
+++ week_04/case_study/CaseStudy3.R
@@ -4,3 +4,4 @@ library(ggplot2)
 filter(gapminder, country != "Kuwait") %>% 
 group_by(continent, year) %>% 
 summarise(gdpPercap=mean(x=gdpPercap, w=pop), pop=sum(as.numeric(pop)))
+ggplot(gapminder, aes(gdpPercap, pop)
