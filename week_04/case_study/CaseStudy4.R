1be0b1e3458029d94d5bdee6736e02be298c9142
Jonathan Lynch <lynch.jonathanm@gmail.com>
2018-10-25 18:50:51 -0400

    10/25 - refresh

--- week_04/case_study/CaseStudy4.R
+++ week_04/case_study/CaseStudy4.R
@@ -3,5 +3,5 @@ library(nycflights13)
 library(dplyr)
 left_join(flights, airports, by = c("dest" = "faa"))
 arrange(flights, desc(distance))
-
+head(flights, n=1)
 #arrange by descending distance, select top row, show name
