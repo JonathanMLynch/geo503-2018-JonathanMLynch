4c9d838667fa3f8c1c4e4fea2089db878c040b57
Jonathan Lynch <lynch.jonathanm@gmail.com>
2018-10-25 18:37:04 -0400

    10/25 - refresh

--- week_04/case_study/CaseStudy3.R
+++ week_04/case_study/CaseStudy3.R
@@ -1,7 +1,15 @@
 library(gapminder)
 library(dplyr)
 library(ggplot2)
-filter(gapminder, country != "Kuwait") %>% 
-group_by(continent, year) %>% 
-summarise(gdpPercap=mean(x=gdpPercap, w=pop), pop=sum(as.numeric(pop)))
-ggplot(gapminder, aes(gdpPercap, pop)
+gapminder <- filter(gapminder, country != "Kuwait") 
+ggplot(gapminder, aes(lifeExp, gdpPercap)) +
+  geom_point(aes(colour = factor(continent), size = pop/100000)) +
+  scale_y_continuous(trans = "sqrt") +    
+  facet_wrap(~year, nrow=1) +
+  theme_bw() +
+  labs(x = "Life Expectancy", y = "GDP per capita")
+group_by(gapminder, continent, year) %>% 
+  summarise(gdpPercap=mean(x=gdpPercap, w=pop), pop=sum(as.numeric(pop)))
+ggplot(gapminder, aes(year, gdpPercap)) +
+  geom_line() +
+  facet_wrap(~continent)
