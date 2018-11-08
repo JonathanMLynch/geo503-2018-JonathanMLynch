04b6e12d78b2b412fe22fa396aa0a3eddad16943
Jonathan Lynch <lynch.jonathanm@gmail.com>
2018-11-08 14:48:55 -0500

    11/8 - class work

--- week_11/class_tasks/11-6 class task.R
+++ week_11/class_tasks/11-6 class task.R
@@ -13,5 +13,6 @@ proj="+proj=cea +lon_0=0 +lat_ts=30 +x_0=0 +y_0=0 +ellps=WGS84 +towgs84=0,0,0,0,
 
 world.trans <- st_transform(world, crs=proj)
 
-result <- foreach(i=world$name_long) %do%
+result <- foreach(i=world.trans$name_long, .combine = rbind) %do%
+  st_is_within_distance(world.trans, world.trans, dist = 100000, sparse = FALSE)
   
